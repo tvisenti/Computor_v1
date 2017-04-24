514fc195f829b9751d50961c9a16d6fcc056e72d
tvisenti <tvisenti@student.42.fr>
2017-04-24 17:45:56 +0200

    [Test]: Add more test

--- calcul.py
+++ calcul.py
@@ -4,10 +4,7 @@ def handleDegree(degree, equa):
 			r = equa[0]
 		if degree == 1:
 			r = (equa[0] / equa[1]) * -1
-		toPrint = str(r)
-		if (toPrint.find('.0') != -1):
-			toPrint = toPrint[:toPrint.find('.0')]
-		print 'The solution is:\n', toPrint
+		print 'The solution is:\n', removeFloatingZero(r)
 	else:
 		r = solverSecondDegree(equa)
 
@@ -15,28 +12,50 @@ def handleDegree(degree, equa):
 def solverSecondDegree(equa):
 	delta = calculDelta(equa[2], equa[1], equa[0])
 	if (delta < 0):
-		print 'No solutions here'
+		print 'Discriminant is strictly negative, the two solutions are:'
+		unrealNb = delta * -1
+		print calculComplexPositif(unrealNb, equa[2], equa[1])
+		print calculComplexNegatif(unrealNb, equa[2], equa[1])
 	elif (delta == 0):
 		print 'Discriminant is equal 0, the only solutions is:'
 		print calculZero(equa[2], equa[1])
 	else:
 		print 'Discriminant is strictly positive, the two solutions are:'
-		print ("%.6f" % calculSupNegative(delta, equa[2], equa[1]))
-		print ("%.6f" % calculSupPositive(delta, equa[2], equa[1]))
+		sqrt = my_sqrt(delta)
+		print ("%.6f" % calculSupNegatif(sqrt, equa[2], equa[1]))
+		print ("%.6f" % calculSupPositif(sqrt, equa[2], equa[1]))
+
+def removeFloatingZero(nb):
+	nb = str(nb)
+	if (nb.find('.0') != -1):
+		nb = nb[:nb.find('.0')]
+	return nb
 
 def calculDelta(a, b, c):
 	return ((b * b) - (4 * a * c))
 
+def calculComplexNegatif(unrealNb, a, b):
+	a = removeFloatingZero(a * 2)
+	b = removeFloatingZero(b * -1)
+	unrealNb = removeFloatingZero(unrealNb)
+	toPrint = '(' + b + ' - i * sqrt(' + unrealNb + ')) / ' + a
+	return toPrint
+
+def calculComplexPositif(unrealNb, a, b):
+	a = removeFloatingZero(a * 2)
+	b = removeFloatingZero(b * -1)
+	unrealNb = removeFloatingZero(unrealNb)
+	toPrint = '(' + b + ' + i * sqrt(' + unrealNb + ')) / ' + a
+	return toPrint
+
 def calculZero(a, b):
 	return (b / (2 * a)) * -1
 
-def calculSupPositive(delta, a, b):
-	sqrt = my_sqrt(delta)
-	return (((b * -1) + sqrt) / (2 * a))
-
-def calculSupNegative(delta, a, b):
-	sqrt = my_sqrt(delta)
+def calculSupNegatif(sqrt, a, b):
 	return (((b * -1) - sqrt) / (2 * a))
+
+def calculSupPositif(sqrt, a, b):
+	return (((b * -1) + sqrt) / (2 * a))
 
 def my_sqrt(number):
 	error = 0.0001
